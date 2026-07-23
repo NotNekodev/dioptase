6f878d58cbb8dd843baf28caf3edfb75c70ab7e1
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2026-07-23 06:19:11 +0200

    feat: unoptimized add test (static function calls too (scarry))

---
+++ tests/AddTest.java
@@ -0,0 +1,10 @@
+public class AddTest {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int main(String[] args) {
+        return add(5, 3);
+    }
+}
