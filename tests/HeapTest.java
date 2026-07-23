fcd5436850953dd59194d7ad9d83715d0465043e
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2026-07-23 08:55:11 +0200

    feat: heap test

---
+++ tests/HeapTest.java
@@ -0,0 +1,13 @@
+class Point {
+
+    int x;
+}
+
+public class HeapTest {
+
+    public static int main(String[] args) {
+        Point p = new Point();
+        p.x = 42;
+        return p.x;
+    }
+}
