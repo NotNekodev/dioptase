0b63c8c86ef9d05a801727bec97d58e8b1eae9ca
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2026-07-24 01:51:29 +0200

    fix: unbreak ObjectArrayTest

--- tests/fixtures/object_array_test/ObjectArrayTest.java
+++ tests/fixtures/object_array_test/ObjectArrayTest.java
@@ -3,7 +3,7 @@ public class ObjectArrayTest {
     public static int main(String[] args) {
         Object[] arr = new Object[3];
         arr[0] = new Object();
-        arr[68] = null;
+        arr[1] = null;
         int count = 0;
         if (arr[0] != null) count++;
         if (arr[1] == null) count++;
