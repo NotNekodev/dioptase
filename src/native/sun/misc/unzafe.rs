bd3f035eebb809c033c854f302a0c1e0e7c8f0e5
NotNekodev <97458908+NotNekodev@users.noreply.github.com>
2026-08-09 04:28:25 +0200

    feat: implement sun/misc/Unsafe.arrayIndexScale
    Signed-off-by: NotNekodev <97458908+NotNekodev@users.noreply.github.com>

--- src/native/sun/misc/unzafe.rs
+++ src/native/sun/misc/unzafe.rs
@@ -57,3 +57,51 @@ pub fn array_base_offset(
 
     Ok(Some(Value::Int(ARRAY_BASE_OFFSET)))
 }
+
+#[native(
+    class = "sun/misc/Unsafe",
+    name = "arrayIndexScale",
+    descriptor = "(Ljava/lang/Class;)I"
+)]
+fn array_index_scale(
+    ctx: &mut NativeContext,
+    args: &[Value],
+) -> Result<Option<Value>, RuntimeError> {
+    let class_obj = match args.get(1) {
+        Some(Value::Reference(Some(r))) => *r,
+        other => {
+            return Err(RuntimeError::Internal(InternalError::InvalidType {
+                class: "sun/misc/Unsafe".to_string(),
+                method: "arrayBaseOffset".to_string(),
+                pc: 0xDEADBEEF,
+                expected: "Reference".to_string(),
+                found: format!("{:?}", other),
+            }));
+        }
+    };
+
+    let class_ref = ctx.vm().heap().get_class_object(class_obj)?;
+    let class = ctx.vm().get_class(class_ref)?;
+
+    let scale = match class.name.as_str() {
+        "[Z" | "[B" => 1,
+        "[C" | "[S" => 2,
+        "[I" | "[F" => 4,
+        "[J" | "[D" => 8,
+
+        // Reference arrays
+        name if name.starts_with("[L") || name.starts_with("[[") => 8,
+
+        other => {
+            return Err(RuntimeError::Internal(InternalError::InvalidType {
+                class: class.name.clone(),
+                method: "Unsafe.arrayIndexScale".to_string(),
+                pc: 0xDEADBEEF,
+                expected: "Descritpor".to_string(),
+                found: format!("{:?}", other),
+            }));
+        }
+    };
+
+    Ok(Some(Value::Int(scale)))
+}
